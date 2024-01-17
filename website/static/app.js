fbc051730199383d7a61549bef42c8b06f50f69a
La-Kiks <kilian.audroin@gmail.com>
2024-01-17 15:55:25 +0100

    Front-End update : search function & styles

--- website/static/app.js
+++ website/static/app.js
@@ -11,3 +11,55 @@ window.onload = function () {
     localStorage.removeItem("scrollPosition"); // Clear after restoration
   }
 };
+
+// no enter on search box + listen on F5
+document.addEventListener('DOMContentLoaded', function () {
+  var searchForm = document.getElementById('search-form');
+  var searchBox = document.getElementById('search-box');
+
+  // Load the search term from localStorage on page load
+  var savedSearch = localStorage.getItem('searchTerm');
+  if (savedSearch) {
+    searchBox.value = savedSearch;
+    search();
+  }
+
+  searchForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form submission
+    search();
+  });
+});
+
+function updateSearch() {
+  var searchBox = document.getElementById('search-box');
+  var searchTerm = searchBox.value;
+
+  // Save the search term to localStorage
+  localStorage.setItem('searchTerm', searchTerm);
+}
+
+function search() {
+  var searchBox = document.getElementById('search-box');
+  var searchText = searchBox.value.toLowerCase();
+  var rows = document.querySelectorAll('table tr');
+
+  rows.forEach(function (row) {
+    var cells = row.querySelectorAll('td');
+    var found = false;
+
+    cells.forEach(function (cell) {
+      var cellText = cell.textContent.toLowerCase();
+
+      if (cellText.includes(searchText)) {
+        found = true;
+      }
+    });
+
+    if (found) {
+      row.style.display = 'table-row';
+    } else {
+      row.style.display = 'none';
+    }
+  });
+}
+
